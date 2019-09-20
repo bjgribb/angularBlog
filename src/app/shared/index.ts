e7ed280235a468c66d81f0af79791d1b015c6dcf
bjgribb <bjgribb@gmail.com>
2019-09-19 23:32:29 -0400

    stripped away back to beginning

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,6 +1,1 @@
-export * from './article-helpers';
-export * from './buttons';
-export * from './layout';
-export * from './list-errors.component';
 export * from './shared.module';
-export * from './show-authed.directive';
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,9 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
-import { ArticleListComponent, ArticleMetaComponent, ArticlePreviewComponent } from './article-helpers';
-import { FavoriteButtonComponent, FollowButtonComponent } from './buttons';
-import { ListErrorsComponent } from './list-errors.component';
-import { ShowAuthedDirective } from './show-authed.directive';
 
 @NgModule({
   imports: [
@@ -18,27 +14,13 @@ import { ShowAuthedDirective } from './show-authed.directive';
     RouterModule
   ],
   declarations: [
-    ArticleListComponent,
-    ArticleMetaComponent,
-    ArticlePreviewComponent,
-    FavoriteButtonComponent,
-    FollowButtonComponent,
-    ListErrorsComponent,
-    ShowAuthedDirective
   ],
   exports: [
-    ArticleListComponent,
-    ArticleMetaComponent,
-    ArticlePreviewComponent,
     CommonModule,
-    FavoriteButtonComponent,
-    FollowButtonComponent,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    ListErrorsComponent,
     RouterModule,
-    ShowAuthedDirective
   ]
 })
 export class SharedModule {}
